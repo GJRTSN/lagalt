44b0c087600d49fb59b5d00e5a9f072a26966b48
MichalPajestka <001michal002@gmail.com>
2023-10-09 14:07:41 +0200

    feat: added profile page and its components

---
+++ client/app/components/ProfileAbout.tsx
@@ -0,0 +1,25 @@
+export default function ProfileAbout() {
+  return (
+    <div className="text-black mt-8">
+      <div id="skills" className="mt-4">
+        <h2 className="text-2xl">Skills</h2>
+        <p className="mt-2">React, Tailwind, Bootstrap, Java</p>
+      </div>
+      <div id="about" className="mt-4">
+        <h2 className="text-2xl">About</h2>
+        <p className="mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget
+          rutrum justo. Nam scelerisque in quam a condimentum. Integer facilisis
+          ex in orci imperdiet malesuada. Phasellus tempor, mi vel sagittis
+          lacinia, lacus eros varius urna, at commodo dui massa et mi.
+        </p>
+        <p className="mt-2">
+          Morbi aliquet velit ante, nec convallis nulla viverra dapibus.
+          Curabitur ut lectus id ligula ullamcorper gravida. Phasellus felis ex,
+          congue ut lorem vitae, vestibulum pellentesque dolor. In fermentum
+          magna sed porta bibendum.
+        </p>
+      </div>
+    </div>
+  );
+}
--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,13 +1,26 @@
+import ProfileAbout from "../components/ProfileAbout";
+import ProfileInfo from "../components/ProfileInfo";
+import ProfileProjects from "../components/ProfileProjects";
+
 export default function Profile() {
   return (
     <div className="h-screen bg-white">
-      <div className="w-full h-12 bg-yellow-500 flex flex-row items-center justify-center ">
-        <p className="text-black">
-          Insert &quot;Profile&quot; related information or actions here
-        </p>
+      <div className="w-full h-12 bg-yellow-500 flex flex-row items-center justify-center">
+        <div className="w-2/4 flex justify-between">
+          <p className="text-black italic py-2">
+            This is how your profile looks like for others
+          </p>
+          <button className="bg-green-500 hover:bg-green-700 font-bold py-2 px-4 rounded-full">
+            Edit
+          </button>
+        </div>
       </div>
-      <div className="h-3/5 flex bg-white flex-col items-center justify-center">
-        <h3 className="text-black text-4xl font-bold ">PROFILE PAGE CONTENT</h3>
+      <div className="flex bg-white flex-col items-center justify-center">
+        <div className="w-2/4">
+          <ProfileInfo />
+          <ProfileAbout />
+          <ProfileProjects />
+        </div>
       </div>
     </div>
   );
