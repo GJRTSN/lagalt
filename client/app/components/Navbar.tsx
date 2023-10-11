0abf9bd8fdba5ab247ce1a3dd77e6f0469ba299c
MichalPajestka <001michal002@gmail.com>
2023-10-11 12:01:01 +0200

    feat: change navbar behaviour based on session

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,47 +1,35 @@
-"use client";
+"use client"
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation"; // Import usePathname from 'next/navigation'
 import logo from "@/public/lagalt_clean.png";
 import usericon from "@/public/user-solid.svg";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 function AuthButton() {
   const { data: session } = useSession();
-  const router = useRouter();
 
   const handleSignOut = async () => {
-    await signOut({ callbackUrl: "/explore" }); // Sign out and redirect to Explore
-    router.push("/explore"); // Manually navigate to Explore page
+    await signOut({ callbackUrl: "/explore" });
   };
 
   if (session) {
     return (
-      <>
-        <button onClick={handleSignOut} className="w-24 h-8 bg-green-700 rounded-md">
+      <button onClick={handleSignOut} className="w-24 h-8 bg-green-700 rounded-md">
         Sign out
       </button>
-      </>
-      
     );
   }
   return (
-    <>
-      <button
-        onClick={() => signIn()}
-        className="w-24 h-8 bg-green-700 rounded-md"
-      >
-        Sign in
-      </button>
-    </>
+    <button onClick={() => signIn()} className="w-24 h-8 bg-green-700 rounded-md">
+      Sign in
+    </button>
   );
 }
 
 export default function Navbar() {
   const pathname = usePathname();
   const { data: session } = useSession();
-
 
   return (
     <div className="w-full h-24 relative bg-[#004080] flex flex-row justify-center gap-12 items-center">
@@ -52,7 +40,7 @@ export default function Navbar() {
         width="70"
         height="20"
       />
-      <p className="mb-8">Friday 06. october 2023</p>
+      <p className="mb-8">Friday 06. October 2023</p>
 
       <div
         id="pagesContainer"
@@ -70,40 +58,37 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <div
-          className={`link transition-colors duration-300 ease-in-out ${
-            pathname === "/projects"
-              ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
-              : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
-          }`}
-        >
-          <Link href="/projects">
-            <h2 className="text-4xl font-lora font-bold">MY PROJECTS</h2>
-          </Link>
-        </div>
+        {session && (
+          <>
+            <div
+              className={`link transition-colors duration-300 ease-in-out ${
+                pathname === "/projects"
+                  ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
+                  : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
+              }`}
+            >
+              <Link href="/projects">
+                <h2 className="text-4xl font-lora font-bold">MY PROJECTS</h2>
+              </Link>
+            </div>
 
-        <div
-          className={`link transition-colors duration-300 ease-in-out ${
-            pathname === "/profile" || pathname === "/profile-edit"
-              ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
-              : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
-          }`}
-        >
-          <Link href="/profile">
-            <h2 className="text-4xl font-lora font-bold">PROFILE</h2>
-          </Link>
-        </div>
+            <div
+              className={`link transition-colors duration-300 ease-in-out ${
+                pathname === "/profile" || pathname === "/profile-edit"
+                  ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
+                  : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
+              }`}
+            >
+              <Link href="/profile">
+                <h2 className="text-4xl font-lora font-bold">PROFILE</h2>
+              </Link>
+            </div>
+          </>
+        )}
       </div>
 
       <div className="flex flex-col absolute right-0 justify-center items-center gap-1 mr-6 text-white">
-        {/* <Image
-          src={usericon}
-          className="absolute left-0 ml-6 transition ease-in-out 1s p-2 rounded shadow-s hover:opacity-70 text-white"
-          alt="explore"
-          width="70"
-          height="20"
-        /> */}
-        {session?.user? (
+        {session?.user ? (
           <>
             <p className="">Logged in as:</p>
             <p className="font-bold">{session.user.name}</p>
