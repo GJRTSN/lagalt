d910078554720df057df42e9402df9684c4e1c40
Jostein Gjertsen <josteingjertsen@gmail.com>
2023-10-12 13:41:26 +0200

    feat: added navigating to explore after signin

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -5,11 +5,15 @@ import logo from "@/public/lagalt_clean.png";
 import Image from "next/image";
 import Link from "next/link";
 
-function AuthButton() {
+export function AuthButton() {
   const { data: session } = useSession();
 
-  const handleSignOut = async () => {
-    await signOut({ callbackUrl: "/explore" });
+  const handleSignOut = () => {
+    signOut({ callbackUrl: "/" });
+  };
+
+  const handleSignIn = () => {
+    signIn(undefined, { callbackUrl: "/explore" });
   };
 
   if (session) {
@@ -23,10 +27,7 @@ function AuthButton() {
     );
   }
   return (
-    <button
-      onClick={() => signIn()}
-      className="w-24 h-8 bg-green-700 rounded-md"
-    >
+    <button onClick={handleSignIn} className="w-24 h-8 bg-green-700 rounded-md">
       Sign in
     </button>
   );
@@ -37,74 +38,80 @@ export default function Navbar() {
   const { data: session } = useSession();
 
   return (
-    <div className="w-full h-24 relative bg-[#004080] flex flex-row justify-center gap-12 items-center">
-      <Image
-        src={logo}
-        className="absolute left-0 ml-6 transition ease-in-out 1s p-2 rounded shadow-s hover:opacity-70"
-        alt="explore"
-        width="70"
-        height="20"
-      />
-      <p className="mb-8">Friday 06. October 2023</p>
+    <>
+      {pathname === "/" ? null : (
+        <div className="w-full h-24 relative bg-[#004080] flex flex-row justify-center gap-12 items-center">
+          <Image
+            src={logo}
+            className="absolute left-0 ml-6 transition ease-in-out 1s p-2 rounded shadow-s hover:opacity-70"
+            alt="explore"
+            width="70"
+            height="20"
+          />
+          <p className="mb-8">Friday 06. October 2023</p>
 
-      <div
-        id="pagesContainer"
-        className="absolute bottom-0 flex flex-row w-3/5 justify-between"
-      >
-        <div
-          className={`link transition-colors duration-300 ease-in-out ${
-            pathname.startsWith("/explore")
-              ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
-              : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
-          }`}
-        >
-          <Link href="/explore">
-            <h2 className="text-4xl font-lora font-bold">EXPLORE</h2>
-          </Link>
+          <div
+            id="pagesContainer"
+            className="absolute bottom-0 flex flex-row w-3/5 justify-between"
+          >
+            <div
+              className={`link transition-colors duration-300 ease-in-out ${
+                pathname.startsWith("/explore")
+                  ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
+                  : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
+              }`}
+            >
+              <Link href="/explore">
+                <h2 className="text-4xl font-lora font-bold">EXPLORE</h2>
+              </Link>
+            </div>
+
+            {session && (
+              <>
+                <div
+                  className={`link transition-colors duration-300 ease-in-out ${
+                    pathname.startsWith("/projects")
+                      ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
+                      : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
+                  }`}
+                >
+                  <Link href="/projects">
+                    <h2 className="text-4xl font-lora font-bold">
+                      MY PROJECTS
+                    </h2>
+                  </Link>
+                </div>
+
+                <div
+                  className={`link transition-colors duration-300 ease-in-out ${
+                    pathname.startsWith("/profile")
+                      ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
+                      : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
+                  }`}
+                >
+                  <Link href="/profile">
+                    <h2 className="text-4xl font-lora font-bold">PROFILE</h2>
+                  </Link>
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="flex flex-col absolute right-0 justify-center items-center gap-1 mr-6 text-white">
+            {session?.user ? (
+              <>
+                <p className="">Logged in as:</p>
+                <p className="font-bold">{session.user.name}</p>
+              </>
+            ) : (
+              <>
+                <p>Want to join?</p>
+              </>
+            )}
+            <AuthButton />
+          </div>
         </div>
-
-        {session && (
-          <>
-            <div
-              className={`link transition-colors duration-300 ease-in-out ${
-                pathname.startsWith("/projects")
-                  ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
-                  : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
-              }`}
-            >
-              <Link href="/projects">
-                <h2 className="text-4xl font-lora font-bold">MY PROJECTS</h2>
-              </Link>
-            </div>
-
-            <div
-              className={`link transition-colors duration-300 ease-in-out ${
-                pathname.startsWith("/profile")
-                  ? "bg-yellow-500 border border-transparent h-3/5 flex items-center rounded-t-md px-4 shadow-inner-md"
-                  : "bg-[#004080] border border-transparent h-3/5 flex items-center rounded-t-md px-4"
-              }`}
-            >
-              <Link href="/profile">
-                <h2 className="text-4xl font-lora font-bold">PROFILE</h2>
-              </Link>
-            </div>
-          </>
-        )}
-      </div>
-
-      <div className="flex flex-col absolute right-0 justify-center items-center gap-1 mr-6 text-white">
-        {session?.user ? (
-          <>
-            <p className="">Logged in as:</p>
-            <p className="font-bold">{session.user.name}</p>
-          </>
-        ) : (
-          <>
-            <p>Want to join?</p>
-          </>
-        )}
-        <AuthButton />
-      </div>
-    </div>
+      )}
+    </>
   );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,20 @@
-import { getServerSession } from "next-auth";
+import { AuthButton } from "./components/Navbar";
+import Link from "next/link";
 
 export default async function Home() {
-  const session = await getServerSession();
-
   return (
-    <>
-      {session?.user?.name ? (
-        <div>{session?.user?.name}</div>
-      ) : (
-        <div>Not logged in</div>
-      )}
-    </>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="mb-4 text-3xl text-gray-800">Welcome!</h1>
+      <p className="mb-2 text-lg text-gray-600">Please log in to continue</p>
+      <AuthButton />
+      <p className="mt-8 mb-2 text-gray-500">
+        or browse as a guest with limited access
+      </p>
+      <Link href="/explore">
+        <button className="py-2 px-4 bg-green-500 text-white rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50">
+          Continue
+        </button>
+      </Link>
+    </div>
   );
 }
